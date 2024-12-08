700d939053d04a35c5aab03f5d7a355c615f715b
suga229 <agussunardi229@gmail.com>
2024-12-08 17:07:57 +0700

    commit

--- app/src/main/java/com/example/wagemproject/MainActivity.java
+++ app/src/main/java/com/example/wagemproject/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.wagemproject;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,53 +15,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Bottom Navigation
+        // Inisialisasi Toolbar elemen
+        EditText searchBar = findViewById(R.id.search_bar);
+        ImageView ivCart = findViewById(R.id.iv_cart);
+        ImageView ivWishlist = findViewById(R.id.iv_wishlist);
+
+        // Event untuk klik pada ikon keranjang
+        ivCart.setOnClickListener(v ->
+                Toast.makeText(MainActivity.this, "Keranjang diklik!", Toast.LENGTH_SHORT).show());
+
+        // Event untuk klik pada ikon wishlist
+        ivWishlist.setOnClickListener(v ->
+                Toast.makeText(MainActivity.this, "Wishlist diklik!", Toast.LENGTH_SHORT).show());
+
+        // Set Bottom Navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        loadFragment(new HomeFragment()); // Default fragment
+
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.nav_home:
-                    Toast.makeText(this, "Home Selected", Toast.LENGTH_SHORT).show();
-                    return true;
+                    selectedFragment = new HomeFragment();
+                    break;
                 case R.id.nav_transaksi:
-                    Toast.makeText(this, "Transaksi Selected", Toast.LENGTH_SHORT).show();
-                    return true;
+                    selectedFragment = new TransactionFragment();
+                    break;
                 case R.id.nav_chat:
-                    Toast.makeText(this, "Chat Selected", Toast.LENGTH_SHORT).show();
-                    return true;
+                    selectedFragment = new ChatFragment();
+                    break;
                 case R.id.nav_profile:
-                    Toast.makeText(this, "Profile Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                default:
-                    return false;
+                    selectedFragment = new ProfileFragment();
+                    break;
             }
+            return loadFragment(selectedFragment);
         });
+    }
 
-        // SearchView
-        SearchView searchView = findViewById(R.id.search_view);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Toast.makeText(MainActivity.this, "Mencari: " + query, Toast.LENGTH_SHORT).show();
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                // Lakukan sesuatu saat teks pencarian berubah
-                return false;
-            }
-        });
-
-        // Wishlist Button
-        ImageView btnWishlist = findViewById(R.id.btn_wishlist);
-        btnWishlist.setOnClickListener(v ->
-                Toast.makeText(this, "Wishlist Clicked", Toast.LENGTH_SHORT).show()
-        );
-
-        // Cart Button
-        ImageView btnCart = findViewById(R.id.btn_cart);
-        btnCart.setOnClickListener(v ->
-                Toast.makeText(this, "Keranjang Clicked", Toast.LENGTH_SHORT).show()
-        );
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
